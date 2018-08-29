b80095faedb446c2b63ea716f3be0fc7d7f889d6
Danny Baumann <dannybaumann@web.de>
2018-08-29 11:04:58 +0200

    soong_config: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,17 +1,21 @@
 package android
 type Product_variables struct {
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
-	Target_uses_color_metadata struct {
-		Cppflags []string
-	}
+        Target_uses_color_metadata struct {
+                Cppflags []string
+        }
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
 }
 
 type ProductVariables struct {
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,13 @@
+add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+
 _contents := $(_contents)    "Fred":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
-$(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
-$(call add_json_bool, Target_uses_color_metadata,            $(filter true,$(TARGET_USES_COLOR_METADATA)))
-$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_str,  Specific_camera_parameter_library,               $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_process_sdk_version_override,    $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str_omitemply,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_uses_color_metadata,                      $(filter true,$(TARGET_USES_COLOR_METADATA)))
+$(call add_json_bool, Uses_generic_camera_parameter_library,           $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
