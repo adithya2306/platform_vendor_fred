8bd85940d91ac005a5dec465cd6e95cfc7010f5a
Jyotiraditya <jyotiraditya182@gmail.com>
2019-01-13 15:49:02 +0530

    [SQUASHED] Add required Includes and Props
    * Build SoundRecored ,tcmiface and others
    * Define default sounds.
    
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -38,7 +38,8 @@ export VENDOR := fred
 # Build Snapdragon apps
 PRODUCT_PACKAGES += \
     SnapdragonGallery \
-    SnapdragonMusic
+    SnapdragonMusic \
+    SoundRecorder
 
 # Include support for GApps backup
 PRODUCT_COPY_FILES += \
@@ -73,3 +74,42 @@ endif
 
 # Include vendor SEPolicy changes
 include vendor/fred/sepolicy/sepolicy.mk
+
+# TCP Connection Management
+PRODUCT_PACKAGES += tcmiface
+PRODUCT_BOOT_JARS += tcmiface
+
+# RCS Service
+PRODUCT_PACKAGES += \
+    rcscommon \
+    rcscommon.xml \
+    rcsservice \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_aidl_static \
+    rcs_service_api \
+    rcs_service_api.xml
+
+# Bluetooth Audio (A2DP)
+PRODUCT_PACKAGES += libbthost_if
+
+# MSIM manual provisioning
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
+# Build WallpaperPicker
+PRODUCT_PACKAGES += WallpaperPicker
+
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += libprotobuf-cpp-full
+
+# Include support for additional filesystems
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    ntfsfix \
+    ntfs-3g
