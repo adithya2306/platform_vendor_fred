529107aa8fb5ee94918fbf2035fc89da887178fa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-01-30 16:24:01 +0530

    scripts: Implement a basic bash script to merge a CAF tag
    * Usage details are mentioned inside the script.

---
+++ scripts/merge-caf-tag.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Basic shell script to merge specified CAF tag in repos tracked by Google's repo tool
+#
+# Usage (in root of ROM source):
+#       repo forall -c bash $(pwd)/vendor/fred/scripts/merge-caf-tag.sh <tag_name> caf
+#
+
+TAG=$1
+CAF_REMOTE=$2
+
+GIT_LINK="https://source.codeaurora.org/quic/la/"
+
+if [[ $REPO_REMOTE != $CAF_REMOTE ]]; then
+
+        # Workaround for build/make as it lies in "platform/build" repo in AOSP/CAF
+        if [[ $REPO_PATH = "build/make" ]]; then REPO_PATH="build"; fi
+            
+        # Check if it is a repo which is forked from AOSP
+        wget -q --spider $GIT_LINK/platform/$REPO_PATH
+
+        if [ $? -eq 0 ]; then
+                # Find branch name from manifest & checkout
+                branch=$(sed 's|refs\/heads\/||' <<< $REPO_RREV)
+                git checkout -q $branch
+
+                # Fetch the tag from AOSP
+                git fetch -q $GIT_LINK/platform/$REPO_PATH $TAG
+
+                # Store the current hash value of HEAD
+                hash=$(git rev-parse HEAD)
+
+                # Merge and inform user on succesful merge, by comparing hash
+                git merge -q --no-ff -m "Merge tag '$TAG' into $branch" FETCH_HEAD 
+                if [ $? -eq 0 ]; then
+                        if [[ $(git rev-parse HEAD) != $hash ]]; then
+                                echo -e "\n\e[34m$REPO_PATH merged succesfully\e[0m\n"
+                        fi
+                else
+                        echo -e "\n\e[31m$REPO_PATH has merge errors\e[0m\n"
+                fi
+        fi
+fi
